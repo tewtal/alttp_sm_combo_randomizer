028b43a43ca16c423eff62592a5051b5ad10f328
Thomas Backmark <total@viskos.org>
2018-11-23 14:32:48 +0100

    Fix mama turtle spring ball check

--- app/Region/SuperMetroid/Maridia/Outer.php
+++ app/Region/SuperMetroid/Maridia/Outer.php
@@ -57,7 +57,7 @@ class Outer extends Region {
 		});
 
         $this->locations["Energy Tank, Mama turtle"]->setRequirements(function($location, $items) {
-            return $items->canFlySM() || $items->has('SpeedBooster') || $items->has('Grapple') || $items->canSpringBallJump();
+            return $items->canFlySM() || $items->has('SpeedBooster') || $items->has('Grapple') || (($items->has('Gravity') || $items->has('HiJump')) && $items->canSpringBallJump());
 		});
 
         $this->can_enter = function($locations, $items) {
