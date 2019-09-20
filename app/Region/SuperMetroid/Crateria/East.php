37d5c754790ad77c3748e0cc8f292b4df447fd72
Michael McKenzie <mikemck101@gmail.com>
2019-09-20 18:35:11 -0230

    Fixed casual marida to east criteria logic

--- app/Region/SuperMetroid/Crateria/East.php
+++ app/Region/SuperMetroid/Crateria/East.php
@@ -84,18 +84,19 @@ class East extends Region {
 
 		$this->locations["Missile (outside Wrecked Ship top)"]->setRequirements(function($location, $items) {
 			return (($items->has('Super') && ($items->has('SpeedBooster') || $items->has('Grapple') || $items->has('SpaceJump') || ($items->has('Gravity') && ($items->canFlySM() || $items->has('HiJump'))))) || $items->canAccessMaridiaPortal())
-				&& ($items->has('HiJump') || $items->canFlySM() || $items->has('SpeedBooster'));
+				&& ($items->has('HiJump') || $items->canFlySM() || $items->has('SpeedBooster'))
+				&& $items->canPassBombPassages();
 		});
 
 		$this->locations["Missile (outside Wrecked Ship middle)"]->setRequirements(function($location, $items) {
 			return ($items->has('SpeedBooster') || $items->has('Grapple') || $items->has('SpaceJump') || ($items->has('Gravity') && ($items->canFlySM() || $items->has('HiJump'))) || $items->canAccessMaridiaPortal())
-			    && $items->has('Super');
+			    && $items->has('Super') && $items->canPassBombPassages();
 		});
 
 		$this->can_enter = function($locations, $items) {
 			return ($items->canUsePowerBombs() && $items->has('Super'))
 				|| ($items->canAccessNorfairPortal() && $items->canUsePowerBombs() && ($items->has('Ice') || $items->has('HiJump') || $items->has('SpaceJump')))
-				|| ($items->canAccessMaridiaPortal() && $items->has('Gravity') && $items->has('Super'));
+				|| ($items->canAccessMaridiaPortal() && $items->has('Gravity') && $items->has('Super') && ($items->canDestroyBombWalls() || $items->canDefeatDraygon()));
 		};
 
 		return $this;
